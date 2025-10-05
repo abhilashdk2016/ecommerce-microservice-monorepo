ad10ca149ef00a0b650360b9e4b2cfb312df8d80
abhilashdk2016 <abhilashdk2014@outlook.com>
2025-10-06 01:24:34 +0530

    Product Service

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,40 @@
+import express, { NextFunction, Request, Response} from 'express';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
+import { shouldBeUser } from './middleware/authMiddleware.js';
+import productRouter from './routes/product.route.js';
+import categoryRouter from './routes/category.route.js';
+
+const app = express();
+app.use(cors({
+    origin: ['http://localhost:3002',
+              'http://localhost:3003'],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get('/healthcheck', (req: Request, res: Response) => {
+    return res.status(200).json({
+      status: "ok",
+      uptime: process.uptime(),
+      timestamp: Date.now()
+    });
+});
+
+app.get("/test", shouldBeUser, (req: Request, res: Response) => {
+    return res.status(200).json({ message: "Product Service Authenticated"});
+});
+
+app.use("/products", productRouter);
+app.use("/categories", categoryRouter);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
+    return res.status(err.status || 500).json({ message: err.message || "Internal Server Server!" });
+});
+
+app.listen(8000, () => {
+    console.log('Product service is running on port 8000');
+});
